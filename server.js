5189e7faec05ba79888cf805ae2de870de18c4c4
Pushkar Pandey <pushkartas2418@gmail.com>
2024-05-28 22:10:28 +0530

    mysql connection

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require("express")
-const app=express()
+const cors = require("cors")
+const mysql=require("mysql")
+const app=express();
 
-const port = 6000;
+app.use(cors())
+const port = 3000;
+
+mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "crud"
+})
+
+app.get('/', (req,res) => {
+    res.json("Backend communicating...")
+})
 
 app.listen(port, ()=> {
     console.log("Running at ", port)
