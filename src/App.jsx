3c275e2e86119010c8afdf957cd2f9a4b946aae3
Pushkar Pandey <pushkartas2418@gmail.com>
2024-06-02 21:32:25 +0530

    implemented update and delete api

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Student from "./component/Student"
 import AddStudent from "./component/AddStudent"
-
+import UpdateStudent from "./component/UpdateStudent"
 function App() {
 
   return (
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Student />}></Route>
         <Route path="/create-student" element={<AddStudent />}></Route>
+        <Route path="/update-student/:id" element={<UpdateStudent />}></Route>
       </Routes>
     </Router >
   )
--- src/component/Student.jsx
+++ src/component/Student.jsx
@@ -1,45 +1,76 @@
-import {useEffect, useState} from "react";
-import axios from "axios"
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 
 const Student = () => {
-    const [studentList, setStudentList]=useState([])
-    useEffect(()=> {
-        axios.get("http://localhost:3000/").then((result) => {
-            console.log(result)
-            if(result.status === 200){
-                setStudentList(result.data)
-            }
-        }).catch((error) => {
-            console.log(error)
-        })
-    },[])
+  const [studentList, setStudentList] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/")
+      .then((result) => {
+        console.log(result);
+        if (result.status === 200) {
+          setStudentList(result.data);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const handleDelete = async (id) => {
+    await axios
+      .delete("http://localhost:3000/delete/student/" + id)
+      .then((result) => {
+        if (result.status === 200) {
+          window.location.reload();
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="d-flex vh-100 justify-content-center align-items-center bg-white">
       <div className="shadow w-50 bg-white rounded p-3">
-        <Link to={"/create-student"} className="btn btn-success">Add +</Link>
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Email</th>
-                <th scope="col">Action</th>
-              </tr>
-            </thead>
-            <tbody>
-                {studentList?.map((data,i) => {
-                    return <><tr key={i}>
+        <Link to={"/create-student"} className="btn btn-success">
+          Add +
+        </Link>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {studentList?.map((data, i) => {
+              return (
+                <>
+                  <tr key={i}>
                     <th scope="row">{data.name}</th>
                     <td>{data.email}</td>
                     <td className="d-flex justify-content-between">
-                        <button className="btn btn-primary">Update</button>
-                        <button className="btn btn-danger">Delete</button>
+                      <Link
+                        to={`/update-student/${data.id}`}
+                        className="btn btn-primary"
+                      >
+                        Update
+                      </Link>
+                      <button
+                        className="btn btn-danger"
+                        onClick={() => handleDelete(data.id)}
+                      >
+                        Delete
+                      </button>
                     </td>
-                  </tr></>
-                })}
-            </tbody>
-          </table>
-        
+                  </tr>
+                </>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     </div>
   );
